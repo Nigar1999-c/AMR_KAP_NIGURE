a408a0e99a7c4018cfd869d1bb0b32f197b0e9aa
Nigar1999-c <absobur8483@gmail.com>
2025-01-06 13:38:02 +0600

    remaining tables done

--- scripts/multi.R
+++ scripts/multi.R
@@ -1,33 +1,95 @@
 # load packages 
-
 library(tidyverse)
 library(easystats)
 library(gtsummary)
 library(gt)
+library(readxl)
+library(dplyr)
+
 
 # load data 
 data <- readxl::read_excel("clean_data/AMR_Clean.xlsx")
 
-# recode QOL_Status 
+
+# recode knowledge level
 data <- data |> 
-  mutate(QOL_Code = case_when(
-    QOL_Status == "Poor" ~ 0, 
-    QOL_Status == "Good" ~ 1
-  ))
+  mutate(antibiotic_knowledge = case_when(
+    knowledge_score <= 50 ~ 0,
+    knowledge_score >50 ~ 1
+  )) |> mutate(across(antibiotic_knowledge, as.factor))
 
-# impact of gender and diagnosis on quality of life(QOL_Score)
-mv_lm <- lm(knowledge_score ~ , data = data)
-report(mv_lm)
+# recode attitude level
+data <- data |> 
+  mutate(attitude_knowledge = case_when(
+    attitude_score <= 50 ~ 0,
+    attitude_score >=51 ~ 1
+  ))|> mutate(across(attitude_knowledge, as.factor))
 
-# tabular form 
-mv_lm |> 
-  tbl_regression() |> 
-  bold_p(t = 0.05)
+# recode practice level
+data <- data |> 
+  mutate(practice_knowledge = case_when(
+    practice_score <= 50 ~ 0,
+    practice_score >=51 ~ 1
+  ))|> mutate(across(practice_knowledge, as.factor))
 
-# impact of gender and diagnos on quality of life status (QOL_Status)
-mv_logreg <- glm(knowledge_score ~ 'Parents age(years)','parents sex' data = data)
-report(mv_logreg)
 
-# tabular 
-mv_logreg |> 
-  tbl_regression(exponentiate = TRUE)
+# Table 4. Factors associated with the level of knowledge among parents of school-going children
+
+
+# multivariate logistic regression
+kn_mv_logreg <- glm(antibiotic_knowledge ~ `Parent’s age (years)`+ `Parent’s sex` +
+                      `Parent’s education level`+ `Employment status` + `Family type` +
+                      `Your average household income per month (BDT)` + `Child’s sex`
+                    + `Child’s age (years)` + `Number of children`,
+                    data = data, family = "binomial")
+report(kn_mv_logreg)
+
+# multivariate logistic regression but in tabular form
+kn_mv_logreg |> 
+  tbl_regression(exponentiate = TRUE) |>
+  bold_p(t = 0.05) |> 
+  as_gt() |> 
+  gtsave("tables/Table5.docx")
+
+
+## Table 5. Factors associated with the level of attitudes towards antibiotic resistance among parents of schoolgoing children 
+
+# multivariate logistic regression
+at_mv_logreg <- glm(attitude_knowledge ~ `Parent’s age (years)`+ `Parent’s sex` +
+                      `Parent’s education level`+ `Employment status` + `Family type` +
+                      `Your average household income per month (BDT)` + `Child’s sex`
+                    + `Child’s age (years)` + `Number of children`,
+                    data = data, family = "binomial")
+report(at_mv_logreg)
+
+# multivariate logistic regression but in tabular form
+at_mv_logreg |> 
+  tbl_regression(exponentiate = TRUE) |>
+  bold_p(t = 0.05) |> 
+  as_gt() |> 
+  gtsave("tables/Table6.docx")
+
+
+
+# Table 6. Factors associated with the level of practices regarding antibiotic resistance among parents of schoolgoing children
+
+# multivariate logistic regression
+pr_mv_logreg <- glm(practice_knowledge ~ `Parent’s age (years)`+ `Parent’s sex` +
+                      `Parent’s education level`+ `Employment status` + `Family type` +
+                      `Your average household income per month (BDT)` + `Child’s sex`
+                    + `Child’s age (years)` + `Number of children`,
+                    data = data, family = "binomial")
+report(pr_mv_logreg)
+
+
+# multivariate logistic regression but in tabular form
+pr_mv_logreg |> 
+  tbl_regression(exponentiate = TRUE) |>
+  bold_p(t = 0.05) |> 
+  as_gt() |> 
+  gtsave("tables/Table7.docx")
+
+
+
+
+
--- scripts/uni.R
+++ scripts/uni.R
@@ -11,22 +11,8 @@ library(gt)
 # load data 
 data <- readxl::read_excel("clean_data/AMR_Clean.xlsx")
 
-# recode QOL_Status 
-data <- data |> 
-  mutate(QOL_Code = case_when(
-    QOL_Status == "Poor" ~ 0, 
-    QOL_Status == "Good" ~ 1
-  ))
 
-# impact of gender on quality of life(QOL_Score)
-uv_lm <- lm(QOL_Score ~ Gender, data = data)
-report(uv_lm)
-
-# impact of gender on quality of life status (QOL_Status)
-uv_logreg <- glm(QOL_Code ~ Gender, data = data, family = "binomial")
-report(uv_logreg)
-
-# Factors associated with quality of life score in the linear regression analysis
+# Factors associated with knowledge score in the linear regression analysis
   data |> 
     select(1:9, knowledge_score) |> 
      tbl_uvregression(
@@ -37,7 +23,7 @@ report(uv_logreg)
        as_gt() |> 
       gtsave("tables/UV_LinReg.docx")
 
-
+  # Factors associated with attitude score in the linear regression analysis
   data |> 
     select(1:9, attitude_score) |> 
     tbl_uvregression(
@@ -48,7 +34,7 @@ report(uv_logreg)
     as_gt() |> 
     gtsave("tables/UV_LinReg1.docx")
   
-  
+  # Factors associated with practice score in the linear regression analysis
   
   data |> 
     select(1:9, practice_score) |> 
@@ -61,17 +47,3 @@ report(uv_logreg)
     gtsave("tables/UV_LinReg2.docx")
   
   
-  
-
-# Factors associated with quality of life score in the univariate logistic regression analysis
-data |> 
-  select(1:19, QOL_Code) |> 
-  tbl_uvregression(
-    method = glm, 
-    method.args = list(family = binomial), 
-    y = QOL_Code, 
-    exponentiate = TRUE
-  ) |> 
-  bold_p(t = 0.05) |> 
-  as_gt() |> 
-  gtsave("tables/UV_LogReg.docx")
